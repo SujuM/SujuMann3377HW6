40c19a0e188f3df5f1a372c86f245c7fb7b9ed2b
SujuM <skm180005@utdallas.edu>
2020-04-27 19:08:30 -0500

    Finishing Phase4

--- program6.cc
+++ program6.cc
@@ -14,7 +14,7 @@
 #include <iostream>
 #include <fstream>
 #include <inttypes.h> 	//For the fixed width types like uint32_t
-#include <string.h>
+#include <string.h> 	//strcpy(), strcat().
 #include "cdk.h"
 #include "binfile.h"
 
@@ -38,13 +38,17 @@ int main()
   int			boxWidths[MATRIX_WIDTH+1] = {BOX_WIDTH, BOX_WIDTH, BOX_WIDTH, BOX_WIDTH};
   int			boxTypes[MATRIX_WIDTH+1] = {vMIXED, vMIXED, vMIXED, vMIXED};
 
+//Those are the buffers to process strings when we display the contents on the Matrix.
   char buffer32[32];
   char buffer64[64];
   char mystr[128];
 
   BinaryFileHeader *myHeader = new BinaryFileHeader();
+  BinaryFileRecord *myRecord = new BinaryFileRecord();
+  
   ifstream binInfile ("/scratch/perkins/cs3377.bin", ios::in | ios::binary);
 
+//Error checking to see the binary file is openable.
   if(!binInfile) {
     cout << "Cannot open the bin file." << endl;
     return 1;
@@ -68,6 +72,7 @@ int main()
 			  MATRIX_NAME_STRING, (char **) rowTitles, (char **) columnTitles, boxWidths,
 				     boxTypes, 1, 1, ' ', ROW, true, true, false);
 
+//Error checking
   if (myMatrix ==NULL)
     {
       printf("Error creating Matrix\n");
@@ -80,12 +85,15 @@ int main()
   /*
    * Dipslay a message
    */
+
+  //Read the header of the binary file.
   binInfile.read((char *) myHeader, sizeof(BinaryFileHeader));
 
+  //Process the header record contents to display on the matrix
   snprintf(buffer32, sizeof(buffer32), "%" PRIx32, myHeader->magicNumber);
   strcpy (mystr, "Magic: 0x");
   strcat (mystr, buffer32);
-  
+  //Set the cell with processed string.
   setCDKMatrixCell(myMatrix, 1, 1, mystr);
 
   snprintf(buffer32, sizeof(buffer32), "%" PRIu32, myHeader->versionNumber);
@@ -100,13 +108,27 @@ int main()
   
   setCDKMatrixCell(myMatrix, 1, 3, mystr);
   
+  //Disply up to the first 4 records in the CDK Matrix
+  for (int i = 0; i < 4; i++) {
+  //Read the record of the binary file contents.
+  	binInfile.read((char *) myRecord, sizeof(BinaryFileRecord));
+  //Process the record contents to display on the matrix
+ 	snprintf(buffer32, sizeof(buffer32), "%" PRIu8, myRecord->strLength);
+  	strcpy (mystr, "strlen: ");
+  	strcat (mystr, buffer32);
+  //Set the cell with processed string.
+  	setCDKMatrixCell(myMatrix,i+2, 1, mystr);
+  	setCDKMatrixCell(myMatrix,i+2, 2, (char *) myRecord->stringBuffer);
+  }
+
+
+//Close the binary file that we opened.
   binInfile.close();
 
   drawCDKMatrix(myMatrix, true);    /* required  */
 
   /* so we can see results */
   sleep (10);
-
 
   // Cleanup screen
   endCDK();
